3d6310f278954a383e7e7d265b21854c6b1cdd59
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-05-05 09:44:41 +0200

    Added api.js for calling apis from my springboot backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Notes from './pages/Notes'
-import Flashcards from './pages/Flashcards'
+import axios from 'axios'
 
-function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-950 text-white font-mono">
-        <Navbar />
-        <main className="max-w-5xl mx-auto px-6 py-10">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/notes" element={<Notes />} />
-            <Route path="/flashcards/:noteId" element={<Flashcards />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  )
-}
+const API = axios.create({
+  baseURL: 'http://localhost:8080/api',
+})
 
-export default App
+export const getNotes = () => API.get('/notes')
+export const createNote = (note) => API.post('/notes', note)
+export const generateFlashcards = (noteId) => API.post(`/flashcards/generate/${noteId}`)
+export const getFlashcards = (noteId) => API.get(`/flashcards/note/${noteId}`)
